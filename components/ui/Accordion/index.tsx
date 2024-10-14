75f068b28a435e9e666718759e5c3156f2385fc0
Lrmdom <lmatiasdomingos@gmail.com>
2024-10-14 16:17:09 +0100

    first try the fork

--- components/ui/Accordion/index.tsx
+++ components/ui/Accordion/index.tsx
@@ -3,6 +3,7 @@ import { ReactNode, useContext } from "react"
 import styled from "styled-components"
 import tw from "twin.macro"
 
+
 import { AccordionContext } from "components/data/AccordionProvider"
 import { AppContext } from "components/data/AppProvider"
 
