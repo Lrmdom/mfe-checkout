48c6536377883ff98044ce36264e087f2ed0ffed
Lrmdom <lmatiasdomingos@gmail.com>
2025-09-04 11:20:48 +0100

    serve is doing js as html

--- next.config.js
+++ next.config.js
@@ -1,24 +1,23 @@
 // @ts-check
-
-const nextBuildId = require("next-build-id")
-
-const shouldAnalyzeBundles = process.env.ANALYZE === "true"
+const nextBuildId = require("next-build-id");
+const shouldAnalyzeBundles = process.env.ANALYZE === "true";
 
 /** @type { import('next').NextConfig } */
 let nextConfig = {
   reactStrictMode: true,
   eslint: {},
   output: process.env.NODE_ENV === "production" ? "export" : "standalone",
-  distDir: "out/dist",
+    //output: "export",
+  distDir: "out/checkout",
   poweredByHeader: false,
   //swcMinify: false,
   webpack: (config) => {
     return config
   },
   // When when app is exported as SPA and served in a sub-folder
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-    ? `${process.env.NEXT_PUBLIC_BASE_PATH}/`
-    : undefined,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+        ? `${process.env.NEXT_PUBLIC_BASE_PATH}`
+        : undefined,
   // https://nextjs.org/docs/api-reference/next.config.js/custom-page-extensions#including-non-page-files-in-the-pages-directory
   pageExtensions: ["page.tsx"],
   generateBuildId: () => nextBuildId({ dir: __dirname }),
